b1a173ee846c1a1368092e3638c83d1cd07007b9
LelouchTwT <jpantoniazitonello@gmail.com>
2024-09-08 22:46:37 -0300

    app done

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <valarray>
+#include <string>
 
 using namespace std;
 
 string add(string x, string y) {
     string result;
-    int n = (int) x.size();
+    int n = max(x.size(), y.size());
     int carry = 0;
+
+    while (x.size() < n) x.insert(x.begin(), '0');
+    while (y.size() < n) y.insert(y.begin(), '0');
 
     for (int i = n - 1; i >= 0; i--) {
         int xBit = x[i] - '0';
         int yBit = y[i] - '0';
 
-        int sum = (xBit ^ yBit ^ carry);
-
-        result.insert(result.begin(), (char) (sum + '0'));
-
-        carry = (xBit & yBit) | (xBit & carry) | (yBit & carry);
+        int sum = xBit + yBit + carry;
+        result.insert(result.begin(), (char)(sum % 2 + '0'));
+        carry = sum / 2;
     }
 
     if (carry) {
@@ -26,22 +27,55 @@ string add(string x, string y) {
     return result;
 }
 
-int makeEqualLength(string &x, string &y) {
-    int n = (int) max(x.size(), y.size());
+string subtract(string x, string y) {
+    string result;
+    int n = max(x.size(), y.size());
+    int borrow = 0;
 
     while (x.size() < n) x.insert(x.begin(), '0');
     while (y.size() < n) y.insert(y.begin(), '0');
 
+    for (int i = n - 1; i >= 0; i--) {
+        int xBit = x[i] - '0';
+        int yBit = y[i] - '0';
+
+        int diff = xBit - yBit - borrow;
+        if (diff >= 0) {
+            result.insert(result.begin(), (char)(diff + '0'));
+            borrow = 0;
+        } else {
+            result.insert(result.begin(), (char)(diff + 2 + '0'));
+            borrow = 1;
+        }
+    }
+
+    result.erase(0, result.find_first_not_of('0'));
+    return result.empty() ? "0" : result;
+}
+
+string shiftLeft(string x, int shift) {
+    for (int i = 0; i < shift; i++) {
+        x.push_back('0');
+    }
+    return x;
+}
+
+int makeEqualLength(string &x, string &y) {
+    int n = max(x.size(), y.size());
+    while (x.size() < n) x.insert(x.begin(), '0');
+    while (y.size() < n) y.insert(y.begin(), '0');
     return n;
 }
 
 string karatsuba(string x, string y) {
     int n = makeEqualLength(x, y);
 
-    if (n == 0) return "0";
+    if (n == 1) {
+        return to_string((x[0] - '0') * (y[0] - '0'));
+    }
 
     int fh = n / 2;
-    int sh = (n - fh);
+    int sh = n - fh;
 
     string xR = x.substr(fh, sh);
     string xL = x.substr(0, fh);
@@ -49,21 +83,23 @@ string karatsuba(string x, string y) {
     string yR = y.substr(fh, sh);
     string yL = y.substr(0, fh);
 
-    string p1 = karatsuba(xR,yR);
-    string p2 = karatsuba(xL,yL);
-    string p3 = karatsuba(add(xR, xL), add(yR,yL));
+    string p1 = karatsuba(xR, yR);
+    string p2 = karatsuba(xL, yL);
+    string p3 = karatsuba(add(xR, xL), add(yR, yL));
 
-    return p1;
+    string result = add(shiftLeft(p2, 2 * sh), shiftLeft(subtract(subtract(p3, p2), p1), sh));
+    result = add(result, p1);
+
+    result.erase(0, result.find_first_not_of('0'));
+
+    return result.empty() ? "0" : result;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc > 3) {
-        cout << "Too many arguments" << endl;
-        return 1;
-    } else if (argc < 3) {
-        cout << "Too few arguments" << endl;
-        return 1;
-    }
-    karatsuba(argv[1], argv[2]);
+
+    string result = karatsuba(argv[1], argv[2]);
+
+    cout << "Product: " << result << endl;
     return 0;
 }
+
